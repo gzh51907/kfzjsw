b086de786b50293fa7ad4d63a0fb81bcce11575d
lws <773286693@qq.com>
2019-10-11 20:59:30 +0800

    合并

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -98,7 +98,6 @@ export default {
   }
 };
 </script>
-<<<<<<< HEAD
 <style lang="scss" scoped>
 .page-cart {
   padding: .2rem;
@@ -143,7 +142,3 @@ header {
   }
 }
 </style>
-=======
-<style scoped>
-</style>
->>>>>>> yd
--- src/pages/Mine.vue
+++ src/pages/Mine.vue
@@ -1,11 +1,39 @@
 <template>
-    <div>mine</div>
+    <div>
+          <header>
+            <i class="el-icon-arrow-left" @click="goOff"></i>
+            个人中心
+            <i class="el-icon-more"></i>
+        </header>
+    </div>
 </template>
 <script>
 export default {
-    
+    data(){
+        return{}
+    },
+    methods:{
+        goOff() {
+      this.$router.go(-1);
+    },
+    }
 }
 </script>
 <style scoped>
-
+header {
+  width: 100%;
+  height: .64rem;
+  background-color: #fff;
+  text-align: center;
+  line-height: .64rem;
+  position: relative;
+  font-size: .213333rem;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 .2rem ;
+  align-items: center;
+}
+header i {
+    font-size: .4rem;
+  }
 </style>
