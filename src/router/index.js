5b8d17aa59320cfeb534bb58db5a64908e32a496
yedi <yedi@qq.com>
2019-10-15 10:22:22 +0800

    上新每日

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,12 @@ import Mine from '../pages/Mine.vue';
 import Book from '../pages/Book.vue';
 import Reg from '../pages/Reg.vue';
 import Login from '../pages/Login.vue';
+<<<<<<< HEAD
 import Newbook from '../pages/everynewbook/newbook.vue';
+=======
+import Details from '../pages/Details.vue';
+
+>>>>>>> 0744388a45f764a437eb3c23ef7441513218ade7
 
 // 3. 实例化router并配置参数
 let router = new VueRouter({
@@ -37,6 +42,11 @@ let router = new VueRouter({
             name: 'cart',
             path: '/cart',
             component: Cart
+        },
+        {
+            name: 'details',
+            path: '/details',
+            component: Details
         },
         {
             name: 'book',
